532d994ad04ae2c4dead58d8f5e2225c452f9169
Al-Taie <agprosup@gmail.com>
2022-06-19 22:08:24 +0300

    fix: optimize code

--- tree.js
+++ tree.js
@@ -37,31 +37,36 @@ function Node(value) {
 }
 
 function drawEdgeAnimated(origin_x, origin_y, destine_x, destine_y, ctx, resolve) {
-    const vertices = [{ x: origin_x, y: origin_y }, { x: destine_x, y: destine_y }]
+    const vertices = [{x: origin_x, y: origin_y}, {x: destine_x, y: destine_y}]
     const N = 35;
-    var waypoints = [];
-    for (var i = 1; i < vertices.length; i++) {
-        var pt0 = vertices[i - 1];
-        var pt1 = vertices[i];
-        var dx = pt1.x - pt0.x;
-        var dy = pt1.y - pt0.y;
-        for (var j = 0; j <= N; j++) {
-            var x = pt0.x + dx * j / N;
-            var y = pt0.y + dy * j / N;
-            waypoints.push({ x: x, y: y });
+    const waypoints = [];
+    for (let i = 1; i < vertices.length; i++) {
+        const pt0 = vertices[i - 1];
+        const pt1 = vertices[i];
+        const dx = pt1.x - pt0.x;
+        const dy = pt1.y - pt0.y;
+        for (let j = 0; j <= N; j++) {
+            const x = pt0.x + dx * j / N;
+            const y = pt0.y + dy * j / N;
+            waypoints.push({x: x, y: y});
         }
     }
-    var t = 1
+    let t = 1;
+
     function resolveCallback(callback) {
         function animate() {
-            if (t < waypoints.length - 1) { requestAnimationFrame(animate) }
-            else { callback() }
+            if (t < waypoints.length - 1) {
+                requestAnimationFrame(animate)
+            } else {
+                callback()
+            }
             ctx.beginPath();
             ctx.moveTo(waypoints[t - 1].x, waypoints[t - 1].y);
             ctx.lineTo(waypoints[t].x, waypoints[t].y);
             ctx.stroke();
             t++;
         }
+
         return animate
     }
 
@@ -69,12 +74,12 @@ function drawEdgeAnimated(origin_x, origin_y, destine_x, destine_y, ctx, resolve
 }
 
 function constructTree(postfix) {
-    const OPERATORS = ['*', '/', '-', '+']
-    var stack = []
-    var root = null;
-    var current;
-    var shift = false;
-    for (var i = postfix.length - 1; i >= 0; i--) {
+    const OPERATORS = ['^', '*', '/', '-', '+']
+    const stack = [];
+    let root = null;
+    let current;
+    let shift = false;
+    for (let i = postfix.length - 1; i >= 0; i--) {
         if (null === root) {
             current = new Node(postfix[i]);
             root = current;
@@ -99,7 +104,8 @@ function constructTree(postfix) {
 }
 
 function getSize(root) {
-    var size = 0
+    let size = 0;
+
     function countSize(root) {
         if (null != root) {
             size++;
@@ -107,23 +113,17 @@ function getSize(root) {
             countSize(root.right)
         }
     }
+
     countSize(root);
     return size;
 }
 
-function print_coords(root) {
-    if (null != root) {
-        print_coords(root.left)
-        console.log(root.value, root.x, root.y)
-        print_coords(root.right)
-    }
-}
-
 function setCoordinates(root) {
-    var i = 0
+    let i = 0;
     const OFFSET = 50
     const size = getSize(root)
     const canvas_mid_point = window.innerWidth / 2;
+
     function setCoordinates(subt, depth) {
         if (null != subt) {
             setCoordinates(subt.left, depth + 1)
@@ -133,6 +133,7 @@ function setCoordinates(root) {
             setCoordinates(subt.right, depth + 1)
         }
     }
+
     setCoordinates(root, 0)
 }
 
@@ -143,10 +144,10 @@ async function drawTree(root, context) {
         if (null != root.left) {
             await new Promise(resolve => root.drawEdge(context, root.left.x, root.left.y, true, resolve))
         }
-        drawTree(root.left, context)
+        await drawTree(root.left, context)
         if (null != root.right) {
             await new Promise(resolve => root.drawEdge(context, root.right.x, root.right.y, false, resolve))
         }
-        drawTree(root.right, context)
+        await drawTree(root.right, context)
     }
 }
